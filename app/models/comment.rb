9bf255c497b907582bb7e1c119480bf25b494559
Jakub Downarowicz <downarowicz.k@gmail.com>
2016-05-05 00:51:02 +0200

    Błędy 2.0

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,10 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
   attr_accessible :user_id, :event_id, :content
+
+  validates :user_id, presence: true
+
+  validates :content, presence: true
+
+  validates :event_id, presence: true
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,11 @@ class Event < ActiveRecord::Base
 
   attr_accessible :user_id, :location, :describe, :discount, :store
   belongs_to :user
-  
+
+  validates :user_id, presence: true
+
+  validates :location, presence: true
+
+  validates :describe, presence: true
+
 end
